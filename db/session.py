4e1bef836c627a1d0b4faab227b40457068564b1
taishakutenn <sunlesslove128@gmail.com>
2025-07-02 01:03:16 +0500

    I put the settings.py file in the config folder and rewrote the import to the database URL in session.py

--- db/session.py
+++ db/session.py
@@ -1,12 +1,10 @@
-from typing import Generator
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-import settings
+from config.settings import REAL_DATABASE_URL
 
 # create async engine
-engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
+engine = create_async_engine(REAL_DATABASE_URL, future=True, echo=True)
 
 # create async session
 session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)

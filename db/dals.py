e16c0f0541484761daeb897088c8578e90996130
Cy_Nec <cy.nec01@gmail.com>
2025-07-03 08:08:22 +0500

    add DALS Group, Cabinet

--- db/dals.py
+++ db/dals.py
@@ -10,12 +10,18 @@ from typing import Union, Tuple, Optional
 from sqlalchemy import select, delete, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models import Teacher
+from models import Teacher, Group, Cabinet
 from config.logging_config import configure_logging
 
 # Создаём объект логгера
 logger = configure_logging()
 
+
+'''
+=================
+DAL's for Teacher
+=================
+'''
 
 class TeacherDAL:
     """Data Access Layer for operating teacher info"""
@@ -88,4 +94,136 @@ class TeacherDAL:
             logger.info(f"У учителя с id: {id} были успешно обновлены поля: {" - ".join(kwargs.keys())}")
             return updated_teacher.id
         logger.warning(f"Не было найдено ни одного учителя с таким id: {id}")
+        return None
+
+
+'''
+===============
+DAL's for Group
+===============
+'''
+
+class GroupDAL:
+    """Data Access Layer for operating group info"""
+
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_group(
+            self, group_name: str, speciality_code: str, quantity_students: int = None, group_advisor_id: int = None, 
+    ) -> Group:
+        new_group = Group(
+            group_name=group_name,
+            speciality_code=speciality_code,
+            quantity_students=quantity_students,
+            group_advisor_id=group_advisor_id,
+        )
+
+        # Add group in session
+        self.db_session.add(new_group)
+
+        # Add changes to the database, but do not commit them strictly
+        await self.db_session.flush()
+        logger.info("Новая группа успешно добавлена в бд")
+        return new_group
+    
+    async def delete_group(self, group_name: str) -> bool:
+        query = delete(Group).where(Group.group_name == group_name).returning(Group.group_name)
+        res = await self.db_session.execute(query)
+        deleted_group = res.fetchone()
+        if not deleted_group:  # if there aren't deleted records
+            logger.warning(f"Не было найдено ни одной группы с таким названием: {group_name}")
+            return False
+        logger.info(f"Группа с названием: '{group_name}' была успешно удалена из бд")
+        return True
+    
+    async def get_group(self, group_name: str) -> Union[str, None]:
+        query = select(Group).where(Group.group_name == group_name)
+        res = await self.db_session.execute(query)  # Make an asynchronous query to the database to search for a group
+        group_row = res.scalar()  # return object Group or None
+        if group_row is not None:
+            logger.info(f"Группа с названием: '{group_name}' была успешно найдена")
+            return group_row.id
+        logger.warning(f"Не было найдено ни одной группы с названием: '{group_name}'")
+        return None
+    
+    async def update(self, group_name: str, **kwargs) -> Optional[str]:
+        query = (
+            update(Group).
+            where(Group.group_name == group_name).
+            values(**kwargs).
+            returning(Group.group_name)
+        )
+        res = await self.db_session.execute(query)
+        update_group = res.scalar()  # return Group name or None
+        if update_group is not None:
+            logger.info(f"Для группы с названием: '{group_name}'. Были успешно обновлены поля: {" - ".join(kwargs.keys())}")
+            return update_group.group_name
+        logger.warning(f"Не было найдено ни одной группы с таким названием: '{group_name}'")
+        return None
+    
+
+'''
+=================
+DAL's for Cabinet
+=================
+'''
+
+class CabinetDAL:
+    """Data Access Layer for operating group info"""
+
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_cabinet(
+            self, cabinet_number: int, capacity: int = None, cabinet_state: str = None, building_number: int = None, 
+    ) -> Cabinet:
+        new_cabinet = Cabinet(
+            cabinet_number=cabinet_number,
+            capacity=capacity,
+            cabinet_state=cabinet_state,
+            building_number=building_number,
+        )
+
+        # Add cabinet in session
+        self.db_session.add(new_cabinet)
+
+        # Add changes to the database, but do not commit them strictly
+        await self.db_session.flush()
+        logger.info("Новый кабинет успешно добавлен в бд")
+        return new_cabinet
+    
+    async def delete_cabinet(self, cabinet_number: int) -> bool:
+        query = delete(Cabinet).where(Cabinet.cabinet_number == cabinet_number).returning(Cabinet.cabinet_number)
+        res = await self.db_session.execute(query)
+        deleted_cabinet = res.fetchone()
+        if not deleted_cabinet:  # if there aren't deleted records
+            logger.warning(f"Не было найдено ни одного кабинета с таким номером: {cabinet_number}")
+            return False
+        logger.info(f"Кабинет с номером: {cabinet_number} был успешно удален из бд")
+        return True
+    
+    async def get_cabinet(self, cabinet_number: int) -> Union[int, None]:
+        query = select(Cabinet).where(Cabinet.cabinet_number == cabinet_number)
+        res = await self.db_session.execute(query)  # Make an asynchronous query to the database to search for a cabinet
+        cabinet_row = res.scalar()  # return object Cabinet or None
+        if cabinet_row is not None:
+            logger.info(f"Кабинет с номером: {cabinet_number} был успешно найден")
+            return cabinet_row.id
+        logger.warning(f"Не было найдено ни одного кабинета с номером: {cabinet_number}")
+        return None
+    
+    async def update(self, cabinet_number: int, **kwargs) -> Optional[int]:
+        query = (
+            update(Cabinet).
+            where(Cabinet.cabinet_number == cabinet_number).
+            values(**kwargs).
+            returning(Cabinet.cabinet_number)
+        )
+        res = await self.db_session.execute(query)
+        update_cabinet = res.scalar()  # return Group name or None
+        if update_cabinet is not None:
+            logger.info(f"Для кабнета с номером: {cabinet_number}. Были успешно обновлены поля: {" - ".join(kwargs.keys())}")
+            return update_cabinet.group_name
+        logger.warning(f"Не было найдено ни одного кабинета с таким номером: {cabinet_number}")
         return None
